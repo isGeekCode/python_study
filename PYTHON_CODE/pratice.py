b79797d0006effe0d0af64a00159aa136d5902a3
wisehyun0814 <ptuguitar@naver.com>
2021-08-19 00:08:48 +0900

    Challenge BMI Calcul

--- PYTHON_CODE/pratice.py
+++ PYTHON_CODE/pratice.py
@@ -1,9 +1,19 @@
-# 변수 count를 0으로 선언
-count = 0
+# 키의 단위 cm를 m로 환산
+a = float(input("키를 입력하세요. : "))*0.01
+b = float(input("몸무게를 입력하세요. : "))
+bmi = b/(a**2)
 
-# if로 반복문 사용
-for i in range(1, 101):
-    # i가 증가하면서 짝수, 동시에 7의 배수가 아닌 값
-    if (i % 2 == 0) and (i % 7 != 0):
-        count += 1
-print("1부터 100까지의 수 중에서 짝수이면서 7의 배수가 아닌 수의 개수는 "+str(count) + "개 입니다")
+# print(bmi)
+print("BMI수치는 ", bmi, "입니다")
+
+if bmi > 25:
+    print("비만입니다.")
+
+elif 23 <= bmi <= 25:
+    print("과체중입니다.")
+
+elif 18.5 <= bmi <= 23:
+    print("정상체중입니다.")
+
+elif bmi < 18.5:
+    print("저체중입니다.")
