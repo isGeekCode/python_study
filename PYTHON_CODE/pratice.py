6591abbdaf3e702ae273b519ae7523a4de05e315
wisehyun0814 <ptuguitar@naver.com>
2021-08-19 14:53:31 +0900

    윤년계산기

--- PYTHON_CODE/pratice.py
+++ PYTHON_CODE/pratice.py
@@ -1,19 +1,8 @@
-# 키의 단위 cm를 m로 환산
-a = float(input("키를 입력하세요. : "))*0.01
-b = float(input("몸무게를 입력하세요. : "))
-bmi = b/(a**2)
 
-# print(bmi)
-print("BMI수치는 ", bmi, "입니다")
+year = int(input("연도를 입력하세요."))
 
-if bmi > 25:
-    print("비만입니다.")
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(str(year) + "년은 윤년입니다.")
 
-elif 23 <= bmi <= 25:
-    print("과체중입니다.")
-
-elif 18.5 <= bmi <= 23:
-    print("정상체중입니다.")
-
-elif bmi < 18.5:
-    print("저체중입니다.")
+else:
+    print(str(year) + "년은 윤년이 아닙니다.")
