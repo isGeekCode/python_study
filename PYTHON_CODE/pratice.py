1d5b2aef9818da1d6599e32879c96e802571fab4
wisehyun0814 <ptuguitar@naver.com>
2021-08-21 10:20:32 +0900

    for문 연습

--- PYTHON_CODE/pratice.py
+++ PYTHON_CODE/pratice.py
@@ -1,8 +1,12 @@
+n = int(input("정수를 입력하세요 : "))
 
-year = int(input("연도를 입력하세요."))
 
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    print(str(year) + "년은 윤년입니다.")
+def getSum(n):
+    a = 0
+    for i in range(n+1):    # 0부터 n까지 나열
+        a += i  # i 만큼 a에 더해진다    ex)a +=1  1만큼 더해진다
+    return a				# a 값을 내보내는 함수
 
-else:
-    print(str(year) + "년은 윤년이 아닙니다.")
+
+result = getSum(n)  # result = a
+print(result)
