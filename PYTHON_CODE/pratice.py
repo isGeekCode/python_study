84151bb7afe204a8ea0a76e51fd10ba9f439c5a8
wisehyun0814 <ptuguitar@naver.com>
2021-08-24 22:09:14 +0900

    tuple & List

--- PYTHON_CODE/pratice.py
+++ PYTHON_CODE/pratice.py
@@ -1,9 +1,13 @@
-print("7개의 수를 입력받고 그 수들의 평균을 구하는 프로그램을 작성해보세요.")
+print("""오늘의 문제 : 튜플과 딕셔너리로 문자열 길이 출력하기
 
-lst = []
+3개의 문자열이 담긴 튜플을 선언하고, 딕셔너리를 통해 각각의 문자열의 길이를 저장한뒤 출력해보세요.
+출력 예시 : {'Hello': 5, 'This is Python': 14, 'Bye': 3}
+""")
 
-for i in range(7):
-    lst.append(int(input("정수를 입력하세요 :")))
+tuple_lst = ("Hello", "This is python", "Bye")
+dict_lst = {}
 
-average = sum(lst)/len(lst)
-print("평균 :", average)
+for i in tuple_lst:
+    dict_lst[i] = len(i)
+
+print("정답 : ", dict_lst)
