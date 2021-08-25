6e80511d5dc5db922566c369d63a9c83dae19f4d
wisehyun0814 <ptuguitar@naver.com>
2021-08-25 14:54:23 +0900

    오늘의 문제 : 계산기 클래스 만들기

--- PYTHON_CODE/pratice.py
+++ PYTHON_CODE/pratice.py
@@ -1,13 +1,28 @@
-print("""오늘의 문제 : 튜플과 딕셔너리로 문자열 길이 출력하기
+# 오늘의 문제 : 계산기 클래스 만들기
+class Calculator:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
 
-3개의 문자열이 담긴 튜플을 선언하고, 딕셔너리를 통해 각각의 문자열의 길이를 저장한뒤 출력해보세요.
-출력 예시 : {'Hello': 5, 'This is Python': 14, 'Bye': 3}
-""")
+    def add(self):
+        result = self.first+self.second
+        return result
 
-tuple_lst = ("Hello", "This is python", "Bye")
-dict_lst = {}
+    def minus(self):
+        result = self.first-self.second
+        return result
 
-for i in tuple_lst:
-    dict_lst[i] = len(i)
+    def mul(self):
+        result = self.first*self.second
+        return result
 
-print("정답 : ", dict_lst)
+    def div(self):
+        result = self.first/self.second
+        return result
+
+
+a = float(input("첫 번째 숫자를 입력해주세요 : "))
+b = float(input("두 번째 숫자를 입력해주세요 : "))
+cal = Calculator(a, b)
+print("덧셈 : ", cal.add(), "뺄셈 : ", cal.minus())
+print("곱셈 : ", cal.mul(), "나눗셈 : ", cal.div())
